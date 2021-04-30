9194b5f3f293c7c230394db3e832ee554d8db130
Ayush Sharma <47772616+ayush5harma@users.noreply.github.com>
2021-04-30 19:48:56 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,16 +1,1 @@
-#!/system/bin/sh
-# Do NOT assume where your module will be located.
-# ALWAYS use $MODDIR if you need to know where this script
-# and module is placed.
-# This will make sure your module will still work
-# if Magisk change its mount point in the future
-MODDIR=${0%/*}
-
-# This script will be executed in late_start service mode
-
-sleep 130
-
-device_config put device_personalization_services 
-AdaptiveAudio__enable_adaptive_audio true
-
 ui_print "- Installing, PixelFeatureDrops"
